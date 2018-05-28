d0bf3c51dc47cf5ce7e0d80191382a62d18aec64
Maciej Szlosarczyk <maciej.szlosarczyk@eestiinternet.ee>
2018-05-28 18:52:45 +0300

    Update search tests

--- test/integration/search_test.rb
+++ test/integration/search_test.rb
@@ -6,18 +6,26 @@ class SearchTest < ActionDispatch::IntegrationTest
     assert(has_field?('domain_name', type: 'text'))
   end
 
-  def test_root_page_returns_404_when_no_domain_is_found
+  def test_root_page_search_returns_404_when_no_domain_is_found
     visit root_path
     fill_in('domain_name', with: 'some-random-domain.ee')
-    click_link_or_button('Submit')
+    click_link_or_button('Search')
     assert_equal(404, page.status_code)
     assert(has_text?('Domain not found: some-random-domain.ee'))
   end
 
-  def test_root_page_returns_200_when_domain_is_found
+  def test_root_page_search_returns_404_for_not_domains
+    visit root_path
+    fill_in('domain_name', with: '1234 ')
+    click_link_or_button('Search')
+    assert_equal(404, page.status_code)
+    assert(has_text?('Domain not found: 1234'))
+  end
+
+  def test_root_page_search_returns_200_when_domain_is_found
     visit root_path
     fill_in('domain_name', with: 'privatedomain.test')
-    click_link_or_button('Submit')
+    click_link_or_button('Search')
     assert_equal(200, page.status_code)
     assert(has_text?('Contact owner'))
     assert(has_text?('name: privatedomain.test'))
