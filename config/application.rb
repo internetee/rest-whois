8258729bcbb5c9c67562064f4fb35bf2f025fd04
Artur Beljajev <artur.beljajev@internet.ee>
2018-05-30 22:51:18 +0300

    Load translations from sub-folders

--- config/application.rb
+++ config/application.rb
@@ -16,9 +16,8 @@ module RestWhois
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
+    config.i18n.default_locale = :en
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
