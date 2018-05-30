02b5a85bdd720cc37ddda0d5fc69dbd2b7f5930d
Artur Beljajev <artur.beljajev@internet.ee>
2018-05-30 20:43:26 +0300

    Set `raise_on_missing_translations` to true in dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,6 @@ Rails.application.configure do
   config.assets.raise_runtime_errors = true
 
   # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
+  config.action_view.raise_on_missing_translations = true
   config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new(ENV['app_name'] || 'rest-whois'))
 end
