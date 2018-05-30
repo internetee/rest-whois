aec2757a0cefe34440a2feeff5fbe5906f5f0ccd
Artur Beljajev <artur.beljajev@internet.ee>
2018-05-30 21:43:19 +0300

    Improve HTML, extract CSS

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,10 @@ module ApplicationHelper
   def other_locales
     allowed_locales - [I18n.locale]
   end
+
+  def body_css_class
+    controller_part = controller_path.split('/').map!(&:dasherize)
+    action_part = action_name.dasherize
+    [controller_part, action_part, 'page'].join('-')
+  end
 end
