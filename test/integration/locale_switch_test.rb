1f87f3c0d347f869109fef65244515a5c0306982
Maciej Szlosarczyk <maciej.szlosarczyk@eestiinternet.ee>
2018-06-07 10:09:13 +0300

    Improve naming of the test class and Russian translation

--- test/integration/locale_switch_test.rb
+++ test/integration/locale_switch_test.rb
@@ -1,10 +1,10 @@
 require 'test_helper'
 
-class LocaleTest < ActionDispatch::IntegrationTest
+class LocaleSwitchTest < ActionDispatch::IntegrationTest
   def test_home_page_has_working_locale_links
     visit root_path
     assert(has_link?('Eesti'))
-    assert(has_link?('по-русски'))
+    assert(has_link?('Русский'))
     click_link_or_button('Eesti')
     assert(has_link?('English'))
   end
