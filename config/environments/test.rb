852aee5846f2316432cb411a21b5a0ebdbb5abb1
Maciej Szlosarczyk <maciej.szlosarczyk@eestiinternet.ee>
2018-06-21 10:02:15 +0300

    Make deprecations raise an error

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -35,8 +35,8 @@ Rails.application.configure do
   # Randomize the order test cases are executed.
   config.active_support.test_order = :random
 
-  # Print deprecation notices to the stderr.
-  config.active_support.deprecation = :stderr
+  # Raise on deprecation errors
+  config.active_support.deprecation = :raise
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
