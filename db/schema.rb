82f35118237e2b85daf74969d7816482ad6bfcd3
Artur Beljajev <artur.beljajev@internet.ee>
2018-11-02 15:07:25 +0200

    Regenerate database schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,34 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180627115124) do
+ActiveRecord::Schema.define(version: 2018_06_27_115124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "contact_requests", force: :cascade do |t|
-    t.integer  "whois_record_id",                 null: false
-    t.string   "secret",                          null: false
-    t.string   "email",                           null: false
-    t.string   "name",                            null: false
-    t.datetime "valid_to",                        null: false
-    t.string   "status",          default: "new", null: false
-    t.inet     "ip_address"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
-    t.index ["email"], name: "index_contact_requests_on_email", using: :btree
-    t.index ["ip_address"], name: "index_contact_requests_on_ip_address", using: :btree
-    t.index ["secret"], name: "index_contact_requests_on_secret", unique: true, using: :btree
-    t.index ["whois_record_id"], name: "index_contact_requests_on_whois_record_id", using: :btree
+    t.integer "whois_record_id", null: false
+    t.string "secret", null: false
+    t.string "email", null: false
+    t.string "name", null: false
+    t.datetime "valid_to", null: false
+    t.string "status", default: "new", null: false
+    t.inet "ip_address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_contact_requests_on_email"
+    t.index ["ip_address"], name: "index_contact_requests_on_ip_address"
+    t.index ["secret"], name: "index_contact_requests_on_secret", unique: true
+    t.index ["whois_record_id"], name: "index_contact_requests_on_whois_record_id"
   end
 
   create_table "whois_records", force: :cascade do |t|
-    t.string   "name"
-    t.text     "body"
-    t.json     "json"
+    t.string "name"
+    t.text "body"
+    t.json "json"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_domains_on_name", using: :btree
+    t.index ["name"], name: "index_domains_on_name"
   end
 
 end
