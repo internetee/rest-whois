5a4689e97e2f2411ffca9bd268b2e04724a76e92
Artur Beljajev <artur.beljajev@internet.ee>
2018-11-07 21:15:13 +0200

    Remove unused `whois_records.body` DB column
    Closes #131

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_27_115124) do
+ActiveRecord::Schema.define(version: 2018_11_02_124618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,6 @@ ActiveRecord::Schema.define(version: 2018_06_27_115124) do
 
   create_table "whois_records", force: :cascade do |t|
     t.string "name"
-    t.text "body"
     t.json "json"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
