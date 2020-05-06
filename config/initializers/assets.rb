74f043981f9354869392ccd9e2353c0587a71d98
Karl Erik Õunapuu <karlerik@kreative.ee>
2020-05-06 15:22:32 +0300

    Load fonts / assets from pipeline

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,12 +7,12 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-Rails.application.config.assets.paths << Rails.root.join('app', 'assets' ,'fonts')
-Rails.application.config.assets.paths << Rails.root.join('app', 'assets' ,'images')
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'images')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w( contact_requests.css )
-Rails.application.config.assets.precompile += %w( fontawesome-all.min.css )
+Rails.application.config.assets.precompile += %w[contact_requests.css]
+Rails.application.config.assets.precompile += %w[fontawesome-all.min.css]
