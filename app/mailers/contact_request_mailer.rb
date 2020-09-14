a9d4d58ae23cbff5e2a5bb4ac25f89f0f39a96fe
Karl Erik Õunapuu <karlerik@kreative.ee>
2020-09-14 14:31:55 +0300

    Use AWS-SES to send contact email if configured

--- app/mailers/contact_request_mailer.rb
+++ app/mailers/contact_request_mailer.rb
@@ -14,6 +14,16 @@ class ContactRequestMailer < ApplicationMailer
   end
 
   def contact_email(contact_request:, recipients:, mail_body:)
+    if ApplicationMailer.ses_configured?
+      ses_contact_email(contact_request: contact_request,
+                        recipients: recipients, mail_body: mail_body)
+    else
+      smtp_contact_email(contact_request: contact_request,
+                         recipients: recipients, mail_body: mail_body)
+    end
+  end
+
+  def smtp_contact_email(contact_request:, recipients:, mail_body:)
     @contact_request = contact_request
     @unsafe_body = mail_body
     @domain = contact_request.whois_record.name
@@ -21,5 +31,35 @@ class ContactRequestMailer < ApplicationMailer
     logger.warn("Contact email sent to #{recipients} from #{@contact_request.email}.")
 
     mail(to: recipients, reply_to: @contact_request.email)
+  end
+
+  def ses_contact_email(contact_request:, recipients:, mail_body:)
+    @contact_request = contact_request
+    @unsafe_body = mail_body
+    @domain = contact_request.whois_record.name
+    @locale = I18n.locale
+    begin
+      resp = Aws::SES::Client.new.send_email({
+        destination: { to_addresses: recipients },
+        message: {
+          body: {
+            text: {
+              charset: 'UTF-8',
+              data: render_to_string(template: "contact_request_mailer/contact_email"),
+            },
+          },
+          subject: {
+            charset: 'UTF-8',
+            data: t('contact_request_mailer.contact_email.subject'),
+          },
+        },
+      source: ENV['mailer_from_address'],
+      })
+      contact_request.update(message_id: resp[:message_id])
+      logger.warn("Contact email sent to #{recipients} from #{@contact_request.email}.")
+
+    rescue Aws::SES::Errors::ServiceError => error
+      logger.warn("Email not sent. Error message: #{error}")
+    end
   end
 end
