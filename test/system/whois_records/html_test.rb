c4e0cdec1b46c4690280b1e52d699939d89bc897
Alex Sherman <yul.golem@gmail.com>
2021-01-06 16:05:16 +0500

    Revert "Move whois API requests to the endpoint"
    This reverts commit 6c04240291501f757ccf6b23d64374f7f7713b57.

--- test/system/whois_records/html_test.rb
+++ test/system/whois_records/html_test.rb
@@ -15,12 +15,12 @@ class PrivatePersonWhoisRecordHTMLTest < ApplicationSystemTestCase
   end
 
   def test_missing_domain
-    visit('/v1/whois/missing-domain.test')
+    visit('/v1/missing-domain.test')
     assert_text('Domain not found: missing-domain.test')
   end
 
   def test_invalid_domain
-    visit('/v1/whois/1.test')
+    visit('/v1/1.test')
     assert_text 'Policy error: 1.test. Please study'
     assert_text 'Requirements for the registration of a Domain Name'
     assert_text 'https://www.internet.ee/domains/ee-domain-regulation#registration-of-domain-names'
@@ -28,18 +28,18 @@ class PrivatePersonWhoisRecordHTMLTest < ApplicationSystemTestCase
 
   def test_contact_form_link_is_visible_when_captcha_is_solved
     solve_captcha
-    visit('/v1/whois/privatedomain.test')
+    visit('/v1/privatedomain.test')
     assert(has_link?('Contact owner'))
   end
 
   def test_contact_form_link_is_visible_when_captcha_is_unsolved
-    visit('v1/whois/privatedomain.test')
+    visit('v1/privatedomain.test')
     refute(has_link?('Contact owner'))
   end
 
   def test_multiple_entries_in_ip_whitelist
     ENV['whitelist_ip'] = '127.0.0.1, 127.0.0.2'
-    visit '/v1/whois/company-domain.test'
+    visit '/v1/company-domain.test'
     assert_no_text 'Not Disclosed'
   end
 end
