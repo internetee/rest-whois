3e2c6c5776d6f2b2a9a22fa3d6d6deda8816d9f2
Keijo Raamat <raamatkeijo@gmail.com>
2021-04-22 16:32:30 +0300

    Bump mimemagic

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-docker build -t ghcr.io/internetee/rest-whois:v1 -f Dockerfile.generic .
+# docker build --no-cache -t ghcr.io/internetee/rest-whois:v1 -f Dockerfile.generic .
 docker run -d \
   -e POSTGRES_PASSWORD=password \
   -e POSTGRES_USERNAME=postgres \
@@ -7,9 +7,12 @@ docker run -d \
 sleep 5
 docker run -d \
   -e RAILS_ENV=test \
+  -e APP_DBHOST=db-r \
   -e PG_USER=postgres \
   -e PG_PASSWORD=password \
   -e PG_DATABASE=postgres \
+  -e recaptcha_site_key='6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI' \
+  -e recaptcha_secret_key='6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe' \
   --name rest-whois --network=test-net ghcr.io/internetee/rest-whois:v1 tail -f /dev/null
 
           
