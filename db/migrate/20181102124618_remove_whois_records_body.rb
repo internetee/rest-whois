4d654aa061ea01419e66d8f34888759c1c552ada
Oleg Hasjanov <oleg.phenomenon@gmail.com>
2021-05-03 10:58:01 +0300

    removed db migrations

--- db/migrate/20181102124618_remove_whois_records_body.rb
+++
@@ -1,9 +0,0 @@
-class RemoveWhoisRecordsBody < ActiveRecord::Migration[5.2]
-  def up
-    # remove_column :whois_records, :body
-  end
-
-  def down
-    # add_column :whois_records, :body, :text
-  end
-end
--- db/migrate/20200914073130_add_message_id_to_contact_request.rb
+++
@@ -1,5 +0,0 @@
-class AddMessageIdToContactRequest < ActiveRecord::Migration[6.0]
-  def change
-    # add_column :contact_requests, :message_id, :string
-  end
-end
