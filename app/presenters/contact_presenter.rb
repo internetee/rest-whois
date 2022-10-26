11d5f8ede575bd66360f344f766518da9165696c
olegphenomenon <oleg.hasjanov@eestiinternet.ee>
2022-10-26 14:36:10 +0300

    added org registrant contact disclosing handler

--- app/presenters/contact_presenter.rb
+++ app/presenters/contact_presenter.rb
@@ -9,16 +9,24 @@ class ContactPresenter
     @whois_record = whois_record
   end
 
+  def registrant_is_org?
+    !whois_record.registrant.private_person? 
+  end
+
+  def registrant_publishable?
+    whois_record.registrant.registrant_publishable
+  end
+
   def name
-    publishable_attribute('name')
+    registrant_is_org? ? disclose_attr_for_org_registrant('name') : publishable_attribute('name')
   end
 
   def email
-    publishable_attribute('email')
+    registrant_is_org? ? disclose_attr_for_org_registrant('email') : publishable_attribute('email')
   end
 
   def phone
-    publishable_attribute('phone')
+    registrant_is_org? ? disclose_attr_for_org_registrant('phone') : publishable_attribute('phone')
   end
 
   def last_update
@@ -32,6 +40,14 @@ class ContactPresenter
   end
 
   private
+
+  def disclose_attr_for_org_registrant(attr)
+    return unless registrant_is_org?
+
+    attr = attr.to_sym
+
+    registrant_publishable? ? contact.send(attr) : disclosable_mask
+  end
 
   def unmasked_attr(attr)
     publication_availability = captcha_solved? || contact_publishable?
